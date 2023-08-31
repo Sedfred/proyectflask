7fb26917b431699ad86906c455a8a81df3c28acb
Jesus Yair Ramirez <a01275404@itesm.mx>
2023-08-31 16:14:11 -0600

    Actualizacion 2

---
+++ servidor.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+#Generar el servidor (Back-End)
+
+servidorWeb = Flask(__name__)
+
+@servidorWeb.route("/holamundo",methods=['GET'])
+def holamundo():
+        return render_template("pagina1.html")
+
+
+if __name__ == "__main__":
+        servidorWeb.run(debug=False,host="0.0.0.0",port="8080")
+
