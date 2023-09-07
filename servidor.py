1dfffcd5857b23b5441e8f3ed4476169801088cf
Sedfred <95438189+Sedfred@users.noreply.github.com>
2023-09-06 18:29:53 -0600

    Add files via upload

--- servidor.py
+++ servidor.py
@@ -1,29 +1,27 @@
-from flask import Flask, render_template, jsonify,request
+from flask import Flask, render_template,jsonify,request
 import numpy as np
 from joblib import load
-import os 
+import os
 
-#cargar modelo
-dt=load('dtl.joblib')
+#Cargar el modelo
+dt=load('dt1.joblib')
 
-#Generar el servidor (Back-End)
-
+#Servidor (backend)
 servidorWeb = Flask(__name__)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
+def formulario():
+    return render_template('pagina1.html')
 
-def holamundo():
-        return render_template("pagina1.html")
-
-#envio de datos a traves de JSON
-
-@servidorWeb.route("/modelo",methods=['POST'])
-def modeloPrediccon():
-        contenido=request.json
-        print(contenido)
-        return jsonify({'resultado':"Hola"})
+#Envio de datos a través de JSON
+@servidorWeb.route('/modelo',methods=['POST'])
+def modeloPrediccion():
+    #Procesar los datos de entrada
+    contenido = request.json
+    print(contenido)
+    return jsonify({'resultado':"Hola"})
 
 
-if __name__ == "__main__":
-        servidorWeb.run(debug=False,host="0.0.0.0",port="8080")
+if __name__ == '__main__':
+    servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
 
